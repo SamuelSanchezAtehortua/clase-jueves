0dfe753ffe09b81c778ec557c1734f9e4950a78b
SamuelSanchezAtehortua <105441967+SamuelSanchezAtehortua@users.noreply.github.com>
2022-09-08 16:41:03 -0500

    Add files via upload

---
+++ Ejercicio De Clase/data/data.js
@@ -0,0 +1,30 @@
+export const data =
+[
+    {
+        "id":1,
+        "name":"Tío Rico",
+        "imagen":"/image/imagen6.jfif"
+    }, 
+    {
+        "id":2,
+        "name":"Tío Rico",
+        "imagen":"/image/imagen2.jpg"
+    },
+    {
+        "id":3,
+        "name":"Tío Rico",
+        "imagen":"/image/imagen3.jfif"
+    },
+    {
+        "id":4,
+        "name":"Tío Rico",
+        "imagen":"/image/imagen4.png"
+    },
+    {
+        "id":5,
+        "name":"Tío Rico",
+        "imagen":"/image/imagen5.jfif"
+    },
+
+    
+]
